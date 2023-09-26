a76c02d83bc69a5cf9673cd810f4187463644f9d
hafidkrntn <hafidkrntn@gmail.com>
2023-09-26 20:48:09 +0700

    first api

---
+++ test/router/router_test.exs
@@ -0,0 +1,16 @@
+defmodule FirstApiTest.RouterTest do
+  use ExUnit.Case, async: true
+
+  use Plug.Test
+
+  @opts FirstApi.Router.init([])
+
+  test "return ok" do
+    build_conn = conn(:get, "/")
+    conn = FirstApi.Router.call(build_conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "OK"
+  end
+end
